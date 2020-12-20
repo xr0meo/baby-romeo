b32394ce002fba87a9e2ac7dceaa9eac0b125808
xromeo <fwillen@gmail.com>
2020-12-20 16:55:38 +0100

    reset, libary not working anymore xd

--- index.js
+++ index.js
@@ -2,30 +2,3 @@ require('dotenv').config()
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-client.music = require("discord.js-musicbot-addon");
-
-client.music.start(client, {
-    youtubeKey: process.env.YT_API_KEY,
-    play: {
-        // Usage text for the help command.
-        usage: "{{prefix}}play some tunes",
-        // Whether or not to exclude the command from the help command.
-        exclude: false  
-    },
-
-    // Make it so anyone in the voice channel can skip the
-    // currently playing song.
-    anyoneCanSkip: true,
-
-    // Make it so the owner (you) bypass permissions for music.
-    ownerOverMember: true,
-    ownerID: "yourDiscordId",
-
-    // The cooldown Object.
-    cooldown: {
-        // This disables the cooldown. Not recommended.
-        enabled: false
-    }
-});
-
-client.login(process.env.TOKEN);
