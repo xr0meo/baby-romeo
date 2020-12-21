60306a3ead3445169079fc7a8aed68fe52254416
xromeo <fwillen@gmail.com>
2020-12-21 11:59:11 +0100

    search for a term and get a link back

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config()
 const Discord = require('discord.js');
+const search = require('youtube-search');
+const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
+
+const searchTerm = 'kodak black skr';
+
+function getVideoUrl(searchTerm) {
+    const opts = {
+        maxResults: 1,
+        key: process.env.YT_KEY
+      };
+      search(searchTerm, opts, function(err, result) {
+        if(err) return console.log(err);
+        console.log(result[0].link)
+      });
+}
+
+getVideoUrl(searchTerm);
